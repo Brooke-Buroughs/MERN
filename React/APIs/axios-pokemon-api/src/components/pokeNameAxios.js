6cffae0df932c3caad2e7f4bb234a72863d32e17
Brooke-Buroughs <107533309+Brooke-Buroughs@users.noreply.github.com>
2022-12-28 19:33:15 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   React/APIs/axios-pokemon-api/src/App.css
    	modified:   React/APIs/axios-pokemon-api/src/App.js
    	new file:   React/APIs/axios-pokemon-api/src/components/pokeNameAxios.js
    
    It took 4.51 seconds to enumerate untracked files. 'status -uno'
    may speed it up, but you have to be careful not to forget to add
    new files yourself (see 'git help status').

---
+++ React/APIs/axios-pokemon-api/src/components/pokeNameAxios.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const PokeName = (props) => {
+    const [pokemon, setPokemon] = useState([]);
+    useEffect(()=>{
+        // axios.get('https://pokeapi.co/api/v2/pokemon?limit=1154')
+        //     .then(response => response.json())
+        //     .then(response=>{setPokemon(response.results)})
+        //     .catch(err => console.log(err))
+    }, []);
+
+    return(
+        <div>
+            {pokemon.length > 0 && pokemon.map((pokemon, index) => {
+                return (<div key={index}> {pokemon.name} </div>)
+            })}
+        </div>
+    )
+}
+
+// const PokeName = (props) => {
+//     const [pokemon, setPokemon] = useState([]);
+
+//     useEffect(() => {
+//         fetch('https://pokeapi.co/api/v2/pokemon?limit=1154')
+//             .then(response => response.json())
+//             .then(response => setPokemon(response.results))
+//             .catch(err => console.log(err))
+//         }, []);
+
+//     return (
+//         <div>
+//             {pokemon.length > 0 && pokemon.map((pokemon, index) => {
+//                 return (<div key={index}> {pokemon.name} </div>)
+//             })}
+//         </div>
+//     );
+// }
+export default PokeName;
