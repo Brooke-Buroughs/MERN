b4fc0a0599156d1ded3fd8d16ac158320b3ebb6f
Brooke-Buroughs <107533309+Brooke-Buroughs@users.noreply.github.com>
2023-01-27 18:11:48 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Express/Faker_API/server.js
    
    It took 6.39 seconds to enumerate untracked files. 'status -uno'
    may speed it up, but you have to be careful not to forget to add
    new files yourself (see 'git help status').

--- Express/Faker_API/server.js
+++ Express/Faker_API/server.js
@@ -1,12 +1,28 @@
+const { faker } = require('@faker-js/faker');
 const express = require("express");
 const app = express();
 const port = 8000;
     
 // req is shorthand for request
 // res is shorthand for response
+const createProduct = () => {
+    const newFake = {
+        name: faker.commerce.productName(),
+        price: "$" + faker.commerce.price(),
+        department: faker.commerce.department()
+    };
+    return newFake;
+};
+
+
+app.get("/api-tester", (req, res) => {
+        res.json(createProduct())
+})
+
 app.get("/api", (req, res) => {
     res.json({ message: "Hello World" });
 });
+
 
 // this needs to be below the other code blocks
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
